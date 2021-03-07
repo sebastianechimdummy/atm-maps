7a41416a872b6ced771c8c166edb03fb0c14693a
Sebastian-Vasile ECHIM <echim.sebastian.v@gmail.com>
2021-03-07 23:58:39 +0200

    Removed styles

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ let map: google.maps.Map;
 
 export function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: {lat: 44.447264, lng: 26.0780039}, // FIA coordinates
+    center: {lat: 44.447264, lng: 26.078004}, // FIA coordinates
     zoom: 15,
     disableDefaultUI: true,
   });
