d5ca5fe26765a3df1f559e1ca03733696253565b
Sebastian-Vasile ECHIM <echim.sebastian.v@gmail.com>
2021-03-08 14:39:06 +0200

    Few changes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ let map: google.maps.Map;
 export function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
     center: {lat: 44.447264, lng: 26.078004}, // FIA coordinates
-    zoom: 15,
+    zoom: 17,
     disableDefaultUI: true,
   });
 }
