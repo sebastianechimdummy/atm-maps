c96cb68bc57574bc9e1178b1417bd938e55bf954
Sebastian-Vasile ECHIM <echim.sebastian.v@gmail.com>
2021-03-08 18:13:50 +0200

    Changed the centering coordinates of the map

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ let map: google.maps.Map;
 
 export function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: {lat: 59.318418, lng: 18.086045}, // The Swedish Museum of Photography coordinates
+    center: {lat: 59.318419, lng: 18.086045}, // The Swedish Museum of Photography coordinates
     zoom: 15,
     disableDefaultUI: true,
   });
